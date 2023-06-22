6406b32432e14d0b4ab16203f9dedf5b6bec1411
amolu <amolujagare@gmail.com>
2023-06-22 20:31:16 +0530

    Initial commit

---
+++ src/main/java/pages/Login.java
@@ -0,0 +1,50 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Login {
+  //  WebDriver driver;
+ //   WebElement element = driver.findElement(By.id("email"));
+
+    @FindBy (id="email")
+    WebElement txtUsername;
+    @FindBy (id="password")
+    WebElement txtPassword;
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement btnLogin;
+
+    @FindBy(xpath = "//a[normalize-space()='I forgot my password']")
+    WebElement forgotPassword;
+    public void clickForgotPassword()
+    {
+        forgotPassword.click();
+    }
+    public Login(WebDriver driver)
+    {
+        PageFactory.initElements(driver,this);
+    }
+
+    public void setTxtUsername(String username)
+    {
+        txtUsername.sendKeys(username);
+    }
+    public void setTxtPassword(String password)
+    {
+        txtPassword.sendKeys(password);
+    }
+    public void clickLogin()
+    {
+        btnLogin.click();
+    }
+
+    public void doLogin(String username,String password)
+    {
+        setTxtUsername(username);
+        setTxtPassword(password);
+        clickLogin();
+    }
+}
